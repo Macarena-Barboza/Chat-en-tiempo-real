66b95974584d91c09ca226b1425e9caa636d33c3
Macarena Barboza <macarenabarbozatri@hotmail.com>
2024-03-16 22:02:11 -0300

    cree carpeta frontend

---
+++ server/db.js
@@ -0,0 +1,12 @@
+// import mysql from "mysql2";
+
+// export const db = mysql.createConnection({
+//     config: {
+//         host: process.env.DB_HOST,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DATABASE,
+//         // port: process.env.DB_PORT,
+//     }
+// })
+
--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from 'node:http';
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
 
+io.on('connection', (socket) => {
+    console.log('usuario conectado')
+})
+
+
+// Chequea si conecto el servidor
 app.get('/', (req, res) => {
     res.send('<h1>Holis</h1>')
 })
 
-app.listen(3001, () => {
-    console.log('server list http://localhost:3001')
+
+
+// Ejecuta el Servidor 
+const port = process.env.PORT ?? 3001
+
+server.listen(port, () => {
+    console.log(`server list http://localhost:${port}`)
 })
 
