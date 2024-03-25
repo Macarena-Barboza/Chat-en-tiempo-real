f7a09533e2a0fcd68eb255ca8652a912fcbacf5a
Macarena Barboza <macarenabarbozatri@hotmail.com>
2024-03-24 21:07:46 -0300

    agregue ultimas funciones y mejore la UI del chat

--- server/index.js
+++ server/index.js
@@ -1,49 +1,49 @@
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from 'node:http';
-import { db } from "./db.js";
-
-
 
 const app = express()
 
 const server = createServer(app)
 const io = new Server(server, {
     cors: {
+        credentials: true,
         origin: '*'
     },
 })
 
-io.on('connection', (socket) => {
-    console.log('usuario Conectado!!!')
-    console.log(socket.id)
+io.on('connection', async (socket) => {
+    console.log(`usuario Conectado!!! ${socket.id}`)
 
-    socket.broadcast.emit('chat', {
-        mensajeChat: ' se conecto',
-        usuario: socket.id
+    socket.on('registro', (user) => {
+        io.emit('registro', user)
     })
 
+    socket.broadcast.emit('chat', {
+        contenido: 'se conecto',
+        id: socket.id,
+        user: socket.id
+    })
 
     socket.on('disconnect', () => {
         console.log('usuario Desconectado...')
+        io.emit('chat', {
+            contenido: 'se desconecto',
+            id: socket.id,
+            user: socket.id
+        })
     })
 
     socket.on('chat', async (msj) => {
-
-
-
         io.emit('chat', msj);
+        console.log(msj)
     })
+
 })
-
-
 
 app.get('/', (req, res) => {
     res.send('<h1>Holis</h1>')
 })
-
-
-
 
 const port = process.env.PORT ?? 3001
 
@@ -51,4 +51,3 @@ server.listen(port, () => {
     console.log(`server list http://localhost:${port}`)
 })
 
-console.log(process.env.PORT)
